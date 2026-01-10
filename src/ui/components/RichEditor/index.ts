e76958a0a848a60dc1b164965f5401042f131ec1
Abdul Latif Mubasir <bj.angel119@gmail.com>
2026-01-10 11:59:15 +0700

    chore: remove draft js lib

--- src/ui/components/RichEditor/index.ts
+++
@@ -1,3 +0,0 @@
-import { RichEditorProps } from "./RichEditor";
-export type { RichEditorProps };
-export { default } from "./RichEditor";
--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,6 +1,5 @@
 export { default as InputGroup } from "./InputGroup";
 export { default as Pagination } from "./Pagination";
-export { default as RichEditor } from "./RichEditor";
 export { default as SelectGroup } from "./SelectGroup";
 export { default as SelectSearchable } from "./SelectSearchable";
 export { default as StatCard } from "./StatCard";
